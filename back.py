6cad5a247bcb3060a00f67f7cd012e1de3696fc1
Shekhar Dixit <shivdixit26@gmail.com>
2021-04-17 16:31:34 +0530

    back-end for contact us form created

--- back.py
+++ back.py
@@ -11,6 +11,16 @@ class Login(db.Model):
     sno = db.Column(db.Integer(), primary_key=True)
     emailid = db.Column(db.String(30), nullable=False)
     password = db.Column(db.String(10), nullable=False)
+
+class Contact(db.Model):
+    __tablename__ = "contact"
+    cid = db.Column(db.Integer(), primary_key=True)
+    cfname = db.Column(db.String(20), nullable=False)
+    clname = db.Column(db.String(20), nullable=False)
+    ctcode = db.Column(db.Integer(), primary_key=True)
+    ctnum = db.Column(db.Integer(), primary_key=True)
+    cemail = db.Column(db.String(30), nullable=False)
+    cfeedback = db.Column(db.String(300), nullable=False)
     
 
 @app.route("/", methods=["GET","POST"])
@@ -31,8 +41,22 @@ def login_page():
 def about():
     return render_template('aboutus.html')
 
-@app.route('/contact')
-def contact():
+@app.route('/contact', methods=["GET","POST"]
+)
+def contact_page():
+    if (request.method == "POST"):
+    	#Fetch entry for database(fetching_data_name= input_name_in_htmlcode)
+        fname = request.form.get('firstname')
+        lname = request.form.get('lastname')
+        tcode = request.form.get('areacode')
+        tnum = request.form.get('telnum')
+        email = request.form.get('emailid')
+        feedback = request.form.get('feedback')
+
+        #Enter fetched data to database (class_Login=fetching_data_name)
+        entry = Contact(cfname=fname, clname=lname, ctcode=tcode, ctnum=tnum, cemail=email, cfeedback=feedback)
+        db.session.add(entry)
+        db.session.commit()
     return render_template('contactus.html')
 
 app.run(debug=True)
